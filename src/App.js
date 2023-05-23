ee1592001294133a96f6e6c26691822c4878297a
Osvaldo <torquati.osvaldo@gmail.com>
2023-05-23 01:30:57 -0300

    Create First Project
    Starting the React JS Course of Prof. Matheus Battisti

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const name = "Osvaldo";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Olá {name}, React</h1>
+        <h2>Criando primeiro projeto React</h2>
+        <h3>Prof. Matheus Battisti</h3>
       </header>
     </div>
   );
