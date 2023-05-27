26880a60f45b9360cd2e50cd2d7aaaee803d1283
Osvaldo <torquati.osvaldo@gmail.com>
2023-05-27 00:34:20 -0300

    Lesson #09 - Event
    Using Events no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import SayMyName from './components/aula05/SayMyName';
-import Pessoa from './components/aula05/Pessoa';
-import List from './components/aula08/List';
+import Evento from './components/aula09/Evento';
+import Form from './components/aula09/Form';
 
 import styles from './App.module.css';
 
 
-function App(props) {
-  const nome = 'Maria';
+function App() {
   return ( 
     <>
     <div className={styles.AppContainer}>
       <h1 className={styles.AppContent}>Testando CSS</h1>
-      <SayMyName nome="Osvaldo" />
-      <SayMyName nome="João" />
-      <SayMyName nome={nome} />
-      <Pessoa 
-        nome="Rodrigo" 
-        idade="28" 
-        foto="https://via.placeholder.com/150" 
-        profissao="Programador" 
-        />
-      <List />
+      <Evento numero={1}/>
+      <Evento numero={2}/>
+      <Form />
     </div>
     </>
    );
