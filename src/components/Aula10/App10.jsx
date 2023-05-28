5bf0232fe708488fa84806b112c25c1f28de28a6
Osvaldo <torquati.osvaldo@gmail.com>
2023-05-28 00:57:56 -0300

    Lesson #11
    Passing events by props

--- src/components/Aula10/App10.jsx
+++ src/components/Aula10/App10.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 //import Evento from './components/aula10/Evento';
+import Form from './components/Aula10/Form';
 
 import styles from './App.module.css';
-import Evento from './components/aula11/Evento'
+
 
 function App() {
   return ( 
     <>
     <div className={styles.AppContainer}>
       <h1 className={styles.AppContent}>Testando CSS</h1>
-      <Evento />
+      <Form />
     </div>
     </>
    );
---
+++ src/components/aula11/Evento.js
@@ -0,0 +1,20 @@
+import Button from '../Eventos/Button';
+
+function Evento() {
+  function meuEvento() {
+    console.log(`Ativando primeiro evento`); 
+  }
+  function segundoEvento() {
+    console.log('Ativando o segundo evento')
+  }
+  
+  return(
+    <>
+      <p>Clique para disparar um evento</p>
+      <Button event={meuEvento} text="Primeiro Evento" />
+      <Button event={segundoEvento} text="Segundo Evento" />
+    </>
+  )
+};
+
+export default Evento;
